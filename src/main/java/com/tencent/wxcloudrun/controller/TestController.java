5fc3821171d7a10dbc97e0200f22bfe3384f7dbd
Carl <13054820156@163.com>
2025-08-17 16:44:04 +0800

    测试接口连通性

--- src/main/java/com/tencent/wxcloudrun/controller/TestController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @description:
@@ -13,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
     @GetMapping("hello")
-    public ApiResponse hello(){
+    public ApiResponse hello(@RequestHeader Map<String, String> header){
+        log.info("headers",header);
         return ApiResponse.ok("hello world");
     }
 }
