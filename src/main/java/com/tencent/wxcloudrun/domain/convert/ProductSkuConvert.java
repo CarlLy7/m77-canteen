719613fc122b80ce6e3aef16ef1a603b052f0e31
Carl <13054820156@163.com>
2025-08-20 20:41:24 +0800

    查询商品sku信息

--- src/main/java/com/tencent/wxcloudrun/domain/convert/ProductSkuConvert.java
+++ src/main/java/com/tencent/wxcloudrun/domain/convert/ProductSkuConvert.java
@@ -3,6 +3,7 @@ package com.tencent.wxcloudrun.domain.convert;
 import com.tencent.wxcloudrun.domain.vo.SkuInfoVo;
 import com.tencent.wxcloudrun.model.BbProductSku;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -18,6 +19,8 @@ public interface ProductSkuConvert {
     ProductSkuConvert INSTANCE=Mappers.getMapper(ProductSkuConvert.class);
 
 
+    @Mapping(source = "skuId",target = "id")
+    @Mapping(source = "skuName",target = "name")
     SkuInfoVo bbProductSkuToSkuInfoVo(BbProductSku bbProductSku);
 
     List<SkuInfoVo> bbProductSkusToSkuInfoVos(List<BbProductSku> bbProductSkus);
